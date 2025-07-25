a24bf383c6f245554823856588ec95c94149d6cf
Intel-dotexe <80932898+Intel-dotexe@users.noreply.github.com>
2025-07-25 17:41:31 +0700

    Add files via upload

---
+++ simple_calculator_v1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main()
+{
+    float input1;
+    float input2;
+    char operator;
+
+    printf("This is a 2 digit simple calculator\n");
+    input1 = get_float("Enter the first number: ");
+    operator = get_char("Enter the operator(+ - * /): ");
+    input2 = get_float("Enter the second number: ");
+
+    if(operator == '+') 
+    {
+        printf("result: %f\n", input1 + input2);
+    } 
+    else if(operator == '-') 
+    {
+        printf("result: %f\n", input1 - input2);
+    } 
+    else if(operator == '*') 
+    {
+        printf("result: %f\n", input1 * input2);
+    } 
+    else if (operator == '/')
+    {
+        printf("result: %f\n", input1 / input2);
+    } 
+    else 
+    {
+        printf("Invalid operator\n");
+    }
+    return 0;
+}
